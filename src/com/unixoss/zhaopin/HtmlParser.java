c41cb00245a3c0b358fdf29d74c9962e82192999
Liu Bing <liubinging@gmail.com>
2012-10-19 16:41:13 +0800

    Changed the tructure and added time and location handling.

--- src/com/unixoss/zhaopin/HtmlParser.java
+++ src/com/unixoss/zhaopin/HtmlParser.java
@@ -7,7 +7,11 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -20,51 +24,70 @@ public class HtmlParser {
 		this.url = new URL(url);
 	}
 	
-	public List<Info> getInfo() throws IOException {
+	public List<Info> getInfo() throws IOException, ParseException {
 		HttpURLConnection conn = (HttpURLConnection)url.openConnection();
 		conn.connect();
 		InputStream stream = conn.getInputStream();
 		return parseItem(stream);
 	}
 	
-	public List<Info> parseItem(InputStream stream) throws IOException {
-		String reType = "^.*rmtimespan[^>]*>\\[(\\w*).*";
-		String reTitle = "\\s*<td class=\"rmTitle\"><a href=\"(([^\"]*)\">(\\w*))</a>";
-		String reTimeLocal = "\\s*<td class=\"rmLocation\">";
+	public List<Info> parseItem(InputStream stream) throws IOException, ParseException {
+		String reType = "^.*rmtimespan.*\\[(\\w*).*";
+		String reTitle = "^.*rmTitle.*href=\"(.*)\">(.*)<.*";
+		String reTimeLocal = "^.*rmLocation.*";
+		String reDateLocal = "^.*<(.*)";
 		Pattern paType = Pattern.compile(reType);
 		Pattern paTitle = Pattern.compile(reTitle);
 		Pattern paTimeLocal = Pattern.compile(reTimeLocal);
+		Pattern paDateLocal = Pattern.compile(reDateLocal);
 		BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-		String line = reader.readLine();
 		List<Info> list = new ArrayList<Info>();
 		Info info = new Info();
-		while(line != null) {
+		String line;
+		while(true) {
+			line = reader.readLine();
+			if(line == null)
+				break;
 			Matcher matcher = paType.matcher(line);
 			if(matcher.matches()) {
-				Log.d("type", line);
-				//Matcher matcher = paType.matcher(line);
 				Log.d("type", matcher.group(1));
-				//info.type = matcher.group(1);
-			} else if(paTitle.matcher(line).matches()) {
-				Log.d("title", line);
-				//Matcher matcher = paTitle.matcher(line);
-				Log.d("link", matcher.group());
+				info.type = matcher.group(1);
+				continue;
+			}
+			matcher = paTitle.matcher(line);
+			if(matcher.matches()) {
 				info.link = matcher.group(1);
-				Log.d("link", matcher.group(0));
+				Log.d("link", matcher.group(1));
+				Log.d("title.", matcher.group(2));
 				info.title = matcher.group(2);
-				Log.d("title.", info.title);
-			} else if(paTimeLocal.matcher(line).matches()) {
-				Log.d("timelocal", line);
+				continue;
+			}
+			matcher = paTimeLocal.matcher(line);
+			if(matcher.matches()) {
 				StringBuilder builder = new StringBuilder();
 				while(!line.contains("</td>")) {
 					line = reader.readLine();
 					builder.append(line);
 				}
+				Log.d("builder1", ""+builder.toString().);
+				String str = builder.toString().replace("\r", "");
+				matcher = paDateLocal.matcher(builder.toString());
+				Log.d("builderbool1", ""+ Pattern.compile(".*").matcher(str).group());
+				Log.d("buildergroup", matcher.group());
+				DateFormat formater = new SimpleDateFormat("MM‘¬dd»’HH:mm");
+				Date date = formater.parse(matcher.group(0));
+				info.start = date.getTime();
+				date = formater.parse(matcher.group(2));
+				info.end = date.getTime();
+				info.location = matcher.group(4);
+				Log.d("start", info.start + "");
+				Log.d("end", info.end + "");
+				Log.d("location", info.location + "");
 				//TODO: add time and location.
 				list.add(info);
 				info = new Info();
+				continue;
 			}
-			line = reader.readLine();
 		}
 		return list;
 	}
