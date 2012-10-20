d276503aa3aca74bb4fb91c82dd88111d08a9d63
Liu Bing <liubinging@gmail.com>
2012-10-20 23:06:08 +0800

    Added time and date gatheration methords to provide these infos.

--- src/com/unixoss/zhaopin/ListAdapter.java
+++ src/com/unixoss/zhaopin/ListAdapter.java
@@ -1,7 +1,10 @@
 package com.unixoss.zhaopin;
 
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,10 +13,12 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 public class ListAdapter extends BaseAdapter {
+	Context context;
 	List<Info> list = null;
 	LayoutInflater inflater;
 	
 	public ListAdapter(Context context) {
+		this.context = context;
 		inflater = LayoutInflater.from(context);
 	}
 
@@ -49,23 +54,72 @@ public class ListAdapter extends BaseAdapter {
 		if(arg1 == null || arg1.getTag() == null) {
 			arg1 = inflater.inflate(R.layout.list_item, null);
 			holder = new ViewHolder();
+			holder.date = (TextView)arg1.findViewById(R.id.date);
 			holder.title = (TextView)arg1.findViewById(R.id.title);
-			holder.timeLocal = (TextView)arg1.findViewById(R.id.time_local);
+			holder.time = (TextView)arg1.findViewById(R.id.time);
+			holder.location = (TextView)arg1.findViewById(R.id.location);
 			arg1.setTag(holder);
 		} else {
 			holder = (ViewHolder)arg1.getTag();
 		}
 		holder.info = getItem(arg0);
-		holder.title.setText(holder.info.type + "  " + holder.info.title);
-		holder.timeLocal.setText(holder.info.start + holder.info.location);
+		//if(holder.info.first) {
+			holder.date.setVisibility(View.VISIBLE);
+			holder.date.setText(getDate(holder.info.start));
+		//}
+		holder.title.setText(String.format(context.getString(
+				R.string.title_format),
+				holder.info.type, holder.info.title));
+		holder.time.setText(String.format(context.getString(
+				R.string.time_format),
+				getTime(holder.info.start), getTime(holder.info.end)));
+		holder.location.setText(String.format(context.getString(
+				R.string.location_format), holder.info.location));
 		arg1.setTag(holder);
 		return arg1;
 	}
 	
+	@TargetApi(9)
+	String getDate(long timeMillis) {
+		String disp = "";
+		Calendar cal = Calendar.getInstance();
+		long today = cal.getTimeInMillis() / 86400000;
+		long curDay = timeMillis / 86400000;
+		if(today == curDay) {
+			disp = context.getString(R.string.today);
+		} else if(curDay - today == 1) {
+			disp = context.getString(R.string.tomorrow);
+		} else {
+			cal.setTimeInMillis(timeMillis);
+			int month = cal.get(Calendar.MONTH) + 1;
+			int day = cal.get(Calendar.DAY_OF_MONTH);
+			disp = month + "ÔÂ" + day + "ÈÕ " + 
+						cal.getDisplayName(Calendar.DAY_OF_WEEK,
+								Calendar.ALL_STYLES, Locale.CHINA);
+		}
+		return disp;
+	}
+	
+	String getTime(long timeMillis) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTimeInMillis(timeMillis);
+		int time = cal.get(Calendar.HOUR_OF_DAY);
+		StringBuilder builder = new StringBuilder();
+		builder.append(time < 10 ? "0" : "");
+		builder.append(time);
+		builder.append(':');
+		time = cal.get(Calendar.MINUTE);
+		builder.append(time < 10 ? "0" : "");
+		builder.append(time);
+		return builder.toString();
+	}
+	
 	class ViewHolder {
 		Info info;
+		TextView date;
 		TextView title;
-		TextView timeLocal;
+		TextView time;
+		TextView location;
 	}
 
 }
