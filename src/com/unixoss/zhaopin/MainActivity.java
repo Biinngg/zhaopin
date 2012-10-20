33662a111d51c89ea00efce690d38c0bcfb9efc5
Liu Bing <liubinging@gmail.com>
2012-10-20 23:07:23 +0800

    Changed the adaper updateing methord to update in the same thread.

--- src/com/unixoss/zhaopin/MainActivity.java
+++ src/com/unixoss/zhaopin/MainActivity.java
@@ -6,18 +6,21 @@ import java.text.ParseException;
 import java.util.List;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.app.Activity;
 import android.view.Menu;
 import android.widget.ListView;
 
 public class MainActivity extends Activity implements Runnable {
 	ListAdapter adapter;
+	List<Info> list;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListAdapter adapter = new ListAdapter(this);
+        adapter = new ListAdapter(this);
         ListView listView = (ListView)findViewById(R.id.listView1);
         listView.setAdapter(adapter);
         Thread thread = new Thread(this);
@@ -30,18 +33,24 @@ public class MainActivity extends Activity implements Runnable {
         return true;
     }
 
+	public Handler mHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			adapter.notifyDataSetChanged(list);
+    	}
+	};
+
 	@Override
 	public void run() {
 		try {
-			HtmlParser parser = new HtmlParser("http://job.ustb.edu.cn/accms/sites/jobc/zhaopinhuixinxi-list.jsp?fromDate=20121029&F_FL1=%E6%A0%A1%E5%86%85&F_FL2=");
-			List<Info> list = parser.getInfo();
-			adapter.notifyDataSetChanged(list);
+			HtmlParser parser = new HtmlParser("http://job.ustb.edu.cn/accms/sites/jobc/zhaopinhuixinxi-list.jsp?F_FL1=%E6%A0%A1%E5%86%85&F_FL2=");
+			list = parser.getInfo();
+			mHandler.sendEmptyMessage(0);
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (ParseException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
